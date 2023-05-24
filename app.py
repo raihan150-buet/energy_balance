1d48a4713585c11fcddb596987dd70628aa39ebe
raihan150-buet <67819099+raihan150-buet@users.noreply.github.com>
2023-05-24 14:40:50 +0600

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ st.markdown("""---""")
 
 
 consumption_by_nocs = (
-    df_selection.groupby(by=["NOCS"]).["Corrected_Consumption"]sum().sort_values(by="Corrected_Consumption")
+    df_selection.groupby(by=["NOCS"]).["Corrected_Consumption"].sum()
 )
 fig_nocs_consumption = px.bar(
     consumption_by_nocs,
