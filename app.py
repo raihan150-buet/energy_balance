706593b50870ce8529601b5dda1a8468f4d66aca
raihan150-buet <67819099+raihan150-buet@users.noreply.github.com>
2023-05-24 14:40:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -19,33 +19,33 @@ def get_data_from_excel():
 
     return df
 
-df_selection = get_data_from_excel()
+df = get_data_from_excel()
 
-# # ---- SIDEBAR ----
-# st.sidebar.header("Please Filter Here:")
-# nocs = st.sidebar.multiselect(
-#     "Select the NOCS:",
-#     options=df["NOCS"].unique(),
-#     default=df["NOCS"].unique()
-# )
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+nocs = st.sidebar.multiselect(
+    "Select the NOCS:",
+    options=df["NOCS"].unique(),
+    default=df["NOCS"].unique()
+)
 
-# substation = st.sidebar.multiselect(
-#     "Select the Substation Name:",
-#     options=df["Substation_Name"].unique(),
-#     default=df["Substation_Name"].unique(),
-# )
+substation = st.sidebar.multiselect(
+    "Select the Substation Name:",
+    options=df["Substation_Name"].unique(),
+    default=df["Substation_Name"].unique(),
+)
 
 
+df_selection = df.query(
+    "NOCS == @nocs & Substation_Name ==@substation")
 
 # ---- MAINPAGE ----
 st.title(":bar_chart: Energy Balance Dashboard")
 st.markdown("##")
 
 # TOP KPI's
-df_selection["Consumption"]=df_selection["Consumption"].astype(int)
-df_selection["Corrected_Consumption"]=df_selection["Corrected_Consumption"].astype(int)
-consumption = df_selection["Consumption"].sum()
-consumption_corrected = df_selection["Corrected_Consumption"].sum()
+consumption = int(df_selection["Consumption"].sum())
+consumption_corrected = int(df_selection["Corrected_Consumption"].sum())
 
 left_column, right_column = st.columns(2)
 with left_column:
@@ -57,8 +57,9 @@ with right_column:
 
 st.markdown("""---""")
 
+
 consumption_by_nocs = (
-    df_selection.groupby(by=["NOCS"])["Corrected_Consumption"].sum()
+    df_selection.groupby(by=["NOCS"]).["Corrected_Consumption"]sum().sort_values(by="Corrected_Consumption")
 )
 fig_nocs_consumption = px.bar(
     consumption_by_nocs,
@@ -74,9 +75,10 @@ fig_nocs_consumption.update_layout(
     xaxis=(dict(showgrid=False))
 )
 
-st.markdown("""---""")
+
+
 st.plotly_chart(fig_nocs_consumption, use_container_width=True)
-st.markdown("""---""")
+
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
@@ -90,6 +92,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.dataframe(df_selection.pivot_table(index=["NOCS"], values=["Consumption","Corrected_Consumption"]))
 st.dataframe(df_selection.pivot_table(index=["Substation_Name"], values=["Consumption","Corrected_Consumption"]))
+
 ss_list=['Moghbazar 132/33/11KV S/S','Moghbazar 33/11KV S/S','Green Road 33/11KV S/S','Lalmatia  33/11KV S/S',
          'Tejgoan 33/11KV S/S','T&T 33/11 KV','Dhanmondi 132/33/11KV S/S','Dhanmondi 33/11KV S/S','Kawranbazar  33/11KV S/S',
          'New Ramna   33/11KV S/S','Ullon 132/33/11KV S/S','Ullon local 33/11KV S/S','Kakrail  33/11KV S/S','Khillgaon  33/11KV S/S',
@@ -109,15 +112,11 @@ ss_list=['Moghbazar 132/33/11KV S/S','Moghbazar 33/11KV S/S','Green Road 33/11KV
          'New Fatullah 33/11 KV SS','P & T 33/11 KV SS','Motijheel 132/33 KV SS','Motijheel 33/11 KV SS (new)','Kazla 132/133 KV SS',
          'Kamalapur Railway 33/11 KV SS','Char Syedpur 132/33KV S/S','Char Syedpur 33/11 KV S/S New','Postogola 132/33 KV S/S'
 ]
-st.markdown("""---""")
 substation_choice = st.selectbox("Pick one Substation from Below",ss_list)
 st.markdown("""---""")
-df_show=df_selection.query("Substation_Name==@substation_choice")
-st.markdown("""---""")
-st.write(df_show[["Substation_Name","Feeder_Name","CF","Opening_Reading","Closing_Reading","Difference","OMF","Consumption","Corrected_Consumption","NOCS"]].astype(str))
-st.markdown("""---""")
+df_show=df.query("Substation_Name==@substation_choice")
+st.write(df_show[["Substation_Name","Feeder_Name","CF","Opening_Reading","Closing_Reading","Difference","OMF","Consumption","Corrected_Consumption","NOCS"]])
 col1, col2, col3= st.columns(3)
-st.markdown("""---""")
 col1.write("Consumption : " + str(df_show["Consumption"].sum()))
 col2.write("Corrected Consumption: " +str(df_show["Corrected_Consumption"].sum()))
 col3.write("Substation Loss: "+str(((df_show["Corrected_Consumption"].sum())-(df_show["Consumption"].sum()))/(df_show["Consumption"].sum())*100)+"%")
@@ -127,14 +126,9 @@ nocs_list=['Motijheel','Khilgaon','Lalbag','Kazla','Postogola','Banglabazar','N.
            'Paribag','Kakrail','Moghbazar','Dhanmondi','Ramna','Shyamoli','Shere b.nagar','Rajarbag','Jigatola','Azimpur','Demra',
            'Matuail','Sytalakhya','Kamrangirchar','Banosree','Adabor'
 ]
-st.markdown("""---""")
 nocs_choice = st.selectbox("Pick one NOCS from Below",nocs_list)
-df_show=df_selection.query("NOCS==@nocs_choice")
-st.write(df_show[["NOCS","Substation_Name","Feeder_Name","Consumption","Corrected_Consumption"]].astype(str))
-st.markdown("""---""")
+df_show=df.query("NOCS==@nocs_choice")
+st.write(df_show[["NOCS","Substation_Name","Feeder_Name","Consumption","Corrected_Consumption"]])
 col1, col2= st.columns(2)
-st.markdown("""---""")
 col1.write("Consumption : " + str(df_show["Consumption"].sum()))
 col2.write("Corrected Consumption: " +str(df_show["Corrected_Consumption"].sum()))
-st.markdown("""---""")
-
