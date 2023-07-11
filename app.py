a2ccf4e9a454cf6d219f2752fbc433349e63cf50
raihan150-buet <67819099+raihan150-buet@users.noreply.github.com>
2023-07-11 16:48:21 +0600

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ consumption_corrected = int(df_selection["Corrected_Consumption"].sum())
 
 left_column,right_column = st.columns(2)
 with left_column:
-    st.subheader("Total Import at 33 KV Level- ")
+    st.subheader("Total Import at 33 KV Level (All NOCS):- ")
 with right_column:
-    st.subheader(f"  All NOCS {consumption_corrected} KWH")
+    st.subheader(f"  {consumption_corrected} KWH")
 
 st.markdown("""----""")
 consumption_by_nocs = (
